7bcd3820e307e9179287e869421e3d3497dc6583
Damián Roháček <rohacekdamian@seznam.cz>
2023-09-12 08:20:21 +0200

    zápisek event - 11.9.23

---
+++ Září/event v konzoli/event/event/Trezor.cs
@@ -0,0 +1,20 @@
+﻿namespace eventbb
+{
+    delegate void Del();
+    internal class Trezor
+    {
+            public event Del Utok; //tohle moc neřešit
+            public bool Otevreni { get; set; } //vytvoří bool a rovnou nastaví gettery a settery
+            public Trezor() {//bezparametrický objekt
+                Otevreni = false;
+            }
+
+        //funkce, ke které budeme přiřazovat funkci v mainu
+            public void otevirani()
+            {
+                Otevreni = true;
+                Utok(); //přiřazovací event
+            }
+        
+    }
+}
