be337c550ba4b109c5fcf798716ef2ddf3c7916c
Damián Roháček <rohacekdamian@seznam.cz>
2023-10-18 14:04:52 +0200

    bb

--- Listopad/barvičky/barvičky/Form1.Designer.cs
+++ Listopad/barvičky/barvičky/Form1.Designer.cs
@@ -54,39 +54,38 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Black;
-            this.panel1.Location = new System.Drawing.Point(91, 281);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Location = new System.Drawing.Point(68, 228);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(443, 246);
+            this.panel1.Size = new System.Drawing.Size(332, 200);
             this.panel1.TabIndex = 0;
             // 
             // hScrollBar1
             // 
             this.hScrollBar1.LargeChange = 1;
-            this.hScrollBar1.Location = new System.Drawing.Point(109, 97);
+            this.hScrollBar1.Location = new System.Drawing.Point(82, 79);
             this.hScrollBar1.Maximum = 255;
             this.hScrollBar1.Name = "hScrollBar1";
-            this.hScrollBar1.Size = new System.Drawing.Size(215, 17);
+            this.hScrollBar1.Size = new System.Drawing.Size(161, 17);
             this.hScrollBar1.TabIndex = 1;
             this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
             // 
             // hScrollBar2
             // 
             this.hScrollBar2.LargeChange = 1;
-            this.hScrollBar2.Location = new System.Drawing.Point(109, 137);
+            this.hScrollBar2.Location = new System.Drawing.Point(82, 111);
             this.hScrollBar2.Maximum = 255;
             this.hScrollBar2.Name = "hScrollBar2";
-            this.hScrollBar2.Size = new System.Drawing.Size(215, 17);
+            this.hScrollBar2.Size = new System.Drawing.Size(161, 17);
             this.hScrollBar2.TabIndex = 2;
             this.hScrollBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar2_Scroll);
             // 
             // hScrollBar3
             // 
             this.hScrollBar3.LargeChange = 1;
-            this.hScrollBar3.Location = new System.Drawing.Point(109, 182);
+            this.hScrollBar3.Location = new System.Drawing.Point(82, 148);
             this.hScrollBar3.Maximum = 255;
             this.hScrollBar3.Name = "hScrollBar3";
-            this.hScrollBar3.Size = new System.Drawing.Size(215, 17);
+            this.hScrollBar3.Size = new System.Drawing.Size(161, 17);
             this.hScrollBar3.TabIndex = 3;
             this.hScrollBar3.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar3_Scroll);
             // 
@@ -94,115 +93,104 @@
             // 
             this.panel2.BackColor = System.Drawing.Color.Red;
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel2.Location = new System.Drawing.Point(35, 82);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Location = new System.Drawing.Point(26, 67);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(39, 35);
+            this.panel2.Size = new System.Drawing.Size(30, 29);
             this.panel2.TabIndex = 4;
             // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel3.Location = new System.Drawing.Point(35, 126);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel3.Location = new System.Drawing.Point(26, 102);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(39, 35);
+            this.panel3.Size = new System.Drawing.Size(30, 29);
             this.panel3.TabIndex = 5;
             // 
             // panel4
             // 
             this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
             this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel4.Location = new System.Drawing.Point(35, 167);
-            this.panel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel4.Location = new System.Drawing.Point(26, 136);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(39, 35);
+            this.panel4.Size = new System.Drawing.Size(30, 29);
             this.panel4.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(356, 97);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(267, 79);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(14, 16);
+            this.label1.Size = new System.Drawing.Size(13, 13);
             this.label1.TabIndex = 6;
             this.label1.Text = "0";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(356, 137);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(267, 111);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(14, 16);
+            this.label2.Size = new System.Drawing.Size(13, 13);
             this.label2.TabIndex = 7;
             this.label2.Text = "0";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(356, 182);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(267, 148);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(14, 16);
+            this.label3.Size = new System.Drawing.Size(13, 13);
             this.label3.TabIndex = 8;
             this.label3.Text = "0";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(431, 97);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(323, 79);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 9;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(431, 137);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Location = new System.Drawing.Point(323, 111);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(132, 22);
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 10;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(431, 178);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Location = new System.Drawing.Point(323, 145);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(132, 22);
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 11;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(431, 222);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox4.Location = new System.Drawing.Point(323, 180);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(132, 22);
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
             this.textBox4.TabIndex = 12;
             this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(356, 225);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(267, 183);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(14, 16);
+            this.label4.Size = new System.Drawing.Size(13, 13);
             this.label4.TabIndex = 13;
             this.label4.Text = "0";
             // 
             // hScrollBar4
             // 
             this.hScrollBar4.LargeChange = 1;
-            this.hScrollBar4.Location = new System.Drawing.Point(109, 220);
+            this.hScrollBar4.Location = new System.Drawing.Point(82, 179);
             this.hScrollBar4.Maximum = 255;
             this.hScrollBar4.Name = "hScrollBar4";
-            this.hScrollBar4.Size = new System.Drawing.Size(215, 17);
+            this.hScrollBar4.Size = new System.Drawing.Size(161, 17);
             this.hScrollBar4.TabIndex = 14;
             this.hScrollBar4.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar4_Scroll);
             // 
@@ -210,39 +198,37 @@
             // 
             this.panel5.BackColor = System.Drawing.Color.Gray;
             this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel5.Location = new System.Drawing.Point(35, 210);
-            this.panel5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel5.Location = new System.Drawing.Point(26, 171);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(39, 35);
+            this.panel5.Size = new System.Drawing.Size(30, 29);
             this.panel5.TabIndex = 15;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Cross;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(819, 158);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(614, 128);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(453, 395);
+            this.pictureBox1.Size = new System.Drawing.Size(340, 321);
             this.pictureBox1.TabIndex = 16;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(892, 126);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(669, 102);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(14, 16);
+            this.label5.Size = new System.Drawing.Size(13, 13);
             this.label5.TabIndex = 17;
             this.label5.Text = "0";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1288, 567);
+            this.ClientSize = new System.Drawing.Size(966, 461);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel5);
@@ -262,7 +248,6 @@
             this.Controls.Add(this.hScrollBar2);
             this.Controls.Add(this.hScrollBar1);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
--- Listopad/barvičky/barvičky/Form1.cs
+++ Listopad/barvičky/barvičky/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
 
 namespace barvičky
 {
@@ -15,39 +16,39 @@ namespace barvičky
         public Form1()
         {
             InitializeComponent();
+            //jestli je cokoliv nepochopitelného, napiš. Šmíd se s tim patlal tak zatraceně dlouho, že jsem začal flexit a redukovat kód na absolutní minimum a vypadá tak :D
+            //TODO: zbavit se intů red, green, blue
         }
-        int red = 0;
+        int red = 0; //pracuji s intama namísto samotných sliderů jako Šmíd, je to trochu čistší
         int green = 0;
         int blue = 0;
+        //když se pohne slider nastaví int na jeho hodnotu a spustí funkci na aktualizování barvy panelu
         private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
         {
             red = e.NewValue;
             set();
-            textBox1.Text = red.ToString();
         }
 
         private void hScrollBar2_Scroll(object sender, ScrollEventArgs e)
         {
             green = e.NewValue;
             set();
-            textBox2.Text = green.ToString();
         }
 
         private void hScrollBar3_Scroll(object sender, ScrollEventArgs e)
         {
             blue = e.NewValue;
             set();
-            textBox3.Text = blue.ToString();
         }
-
+        //
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             try
             {
                 red = Convert.ToInt32(textBox1.Text);
             }catch { red = 0; }
-            red = numCheck(red);
-            set();
+            red = numCheck(red);//moje vlastní funkce, je zapsaná níže
+            set();//taky moje vlastní funkce, nastavuje barvu toho panelu
             hScrollBar1.Value = red;
         }
 
@@ -65,43 +66,40 @@ namespace barvičky
 
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
-            try
-            {
-                blue = Convert.ToInt32(textBox3.Text);
-            }
+            try { blue = Convert.ToInt32(textBox3.Text); }
             catch { blue = 0; }
             blue = numCheck(blue);
             set();
             hScrollBar3.Value = blue;
             
         }
+        //kontroluje vložené číslo do text baru jestli je hodnota rgb, když není vrátí nejbližší správnou
         int numCheck(int num)
         {
-            if (num > 255)
-            {
-                num = 255;
-            }
-            else if (num < 0)
-            {
-                num = 0;
-            }
-            return num;
+            if (num > 255) {return 255;}
+            /*hezčí if (kdyžtak mi o tomhle písni xd)
+                   podmínka    true :  false*/
+            return (num < 0) ?    0 :  num;
+            //vrací zkontrolovanou a případně přepsanou hodnotu num (čísla, co jsme tam vložili)
         }
+        //akorát nastavuje barvy panelu, nechtělo se mi to kopírovat tak je to funkce :D
         void set()
         {
-            panel1.BackColor = Color.FromArgb(255, red, green, blue);
-            label1.Text = red.ToString(); label2.Text = green.ToString(); label3.Text = blue.ToString();
+            panel1.BackColor = Color.FromArgb(255, red, green, blue); 
+            label1.Text = red.ToString(); label2.Text = green.ToString(); label3.Text = blue.ToString();//nastavuje texty za slidery na hodnoty čísel
+            textBox1.Text = red.ToString(); textBox2.Text = green.ToString(); textBox3.Text = blue.ToString(); //nastavuje text v editovatelných text boxech
+            hScrollBar1.Value = red; hScrollBar2.Value = green; hScrollBar3.Value = blue;
         }
-
+        //speciální slider na šedou. hýbe se všemi třemi slidry najednou
         private void textBox4_TextChanged(object sender, EventArgs e)
         {
-            try
+            try //try catch proto, aby nespadl program po vložení písmenka
             {
                 blue = Convert.ToInt32(textBox4.Text); blue = numCheck(blue);
                 green = Convert.ToInt32(textBox4.Text);green = numCheck(green);
                 red = Convert.ToInt32(textBox4.Text); red = numCheck(red);
             }
-            catch { blue = 0; red = 0; green = 0; }
+            catch { blue = 0; red = 0; green = 0; } //pokud se tak stane, je to černá
             set();
         }
 
@@ -113,28 +111,47 @@ namespace barvičky
             set();
             label4.Text = Convert.ToString(e.NewValue);
         }
-        private Color GetColorAt(Point point)
-        {
-            return ((Bitmap)pictureBox1.Image).GetPixel(point.X, point.Y);
-        }
 
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
-            Bitmap harambe = new Bitmap(pictureBox1.Image);
-            Color pixelColor;
-            pixelColor = GetColorAt(e.Location);
+            /*red = ((Bitmap)pictureBox1.Image).GetPixel(e.X, e.Y).R; green = ((Bitmap)pictureBox1.Image).GetPixel(e.X, e.Y).G; blue = ((Bitmap)pictureBox1.Image).GetPixel(e.X, e.Y).B;
+            set();*/
             label5.Text = "X: " + e.X + " Y: " + e.Y;
-            red = pixelColor.R;
-            green = pixelColor.G;
-            blue = pixelColor.B;
-            set();
-            hScrollBar1.Value = red;
-            hScrollBar2.Value = green;
-            hScrollBar3.Value = blue;
-            textBox1.Text = red.ToString();
-            textBox2.Text = green.ToString();
-            textBox3.Text = blue.ToString();
         }
-        //getPixel
+
+        int startX = 0;
+        int startY = 0;
+        int bb = 0;
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Graphics graphics = Graphics.FromImage(pictureBox1.Image);
+            if (bb == 0)
+            {
+                Bitmap b = new Bitmap(pictureBox1.Image);
+                pictureBox1.Image = b;
+                bb++;
+            }
+            Pen pero = new Pen(Color.FromArgb(255, red, green, blue));
+            if (e.Button == MouseButtons.Left && startX == 0)
+            {
+                startX = e.X;
+                startY = e.Y;
+            }
+            if (e.Button == MouseButtons.Left)
+            {
+                //graphics.DrawLine(pero, startX, startY, e.X, e.Y);
+                using (var graphics = Graphics.FromImage(b))
+                {
+                    graphics.DrawLine(pero, startX, startY, e.X, e.Y);
+                }
+                startX = e.X;
+                startY = e.Y;
+            }
+            if (e.Button == MouseButtons.None)
+            {
+                startX = 0;
+                startY = 0;
+            }
+        }
     }
 }
