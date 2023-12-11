9ab419798ccdfdf5244108aefdd55e5acfb3eabb
Damián Roháček <rohacekdamian@seznam.cz>
2023-12-11 08:26:18 +0100

    semaf

---
+++ Prosinec/semafor/semafor/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace semafor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Location = new System.Drawing.Point(326, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(100, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(326, 155);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(100, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Cursor = System.Windows.Forms.Cursors.PanWest;
+            this.panel3.Location = new System.Drawing.Point(326, 261);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(100, 100);
+            this.panel3.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ Prosinec/semafor/semafor/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace semafor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            reset();//aby se spuštěním aplikace byly všechny panely černé
+        }
+        //po každém ticku se číslo zvyšuje až do 5
+        public int count = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //pokud je k danému číslu přiřazená změna
+            switch (count)
+            {
+                case 0: //když je číslo 0 tak se první panel nastaví na červenou
+                    reset();//všechny černé
+                    panel1.BackColor = Color.Red;//červená
+                    break;
+                case 1:
+                    panel2.BackColor = Color.Orange; 
+                    break;
+                case 2:
+                    reset();
+                    panel3.BackColor = Color.Green;
+                    break;
+                case 4:
+                    reset();
+                    panel2.BackColor = Color.Orange; 
+                    break;
+                case 5:
+                    reset();
+                    panel1.BackColor = Color.Red;
+                    count = 0;//nastavuju číslo na 0 aby byly dva ticky červené
+                    break;
+            }
+            count++;
+        }
+        //funkce na nastavení všech tří panelů na černou
+        void reset()
+        {
+            panel1.BackColor = Color.Black;
+            panel2.BackColor = Color.Black;
+            panel3.BackColor = Color.Black;
+        }
+    }
+}
